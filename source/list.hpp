fbfdc2dba6823a94bb25a4dbae8b60b114c9c00c
Nyran <adrian.kreskowski@uni-weimar.de>
2020-05-29 22:43:41 +0200

    commit initial state of Aufgabenblatt 3

---
+++ source/list.hpp
@@ -0,0 +1,277 @@
+#ifndef BUW_LIST_HPP
+#define BUW_LIST_HPP
+#include <cassert>
+#include <cstddef>  //ptrdiff_t
+#include <iterator> //std::bidirectional_iterator_tag
+#include <iostream>
+
+#include <initializer_list>
+
+template <typename T>
+class List;
+
+template <typename T>
+struct ListNode {
+  T         value = T{};
+  ListNode* prev = nullptr;
+  ListNode* next = nullptr;
+};
+
+
+//TODO: Implementierung der Methoden des Iterators 
+//      (nach Vorlesung STL-1 am 09. Juni) (Aufgabe 3.11)
+template <typename T>
+struct ListIterator {
+  using Self              = ListIterator<T>;
+  using value_type        = T;
+  using pointer           = T*;
+  using reference         = T&;
+  using difference_type   = ptrdiff_t;
+  using iterator_category = std::bidirectional_iterator_tag;
+
+
+  /* DESCRIPTION  operator*() */
+  T&  operator*()  const {
+    if(nullptr == node) {
+      throw "Iterator does not point to valid node";
+    }
+
+    //TODO: remaining implementation of derefenciation of 
+    //      iterator using operator* (Aufgabe 3.11 - Teil 1)
+
+  } //call *it
+
+  /* DESCRIPTION  operator->() */
+  T* operator->() const {
+    if(nullptr == node) {
+      throw "Iterator does not point to valid node";
+    }
+
+    //TODO: remaining implementation of derefenciation of 
+    //      iterator using operator-> (Aufgabe 3.11 - Teil 2)
+  }  //call it->method() or it->member
+
+
+  /* PREINCREMENT, call: ++it, advances one element forward */
+  ListIterator<T>& operator++() {
+    if(nullptr == node) {
+      throw "Iterator does not point to valid node";
+    }
+
+    //TODO: Implement Postincrement-Operation for Iterator
+    //      (Aufgabe 3.11 - Teil 3)
+    
+  }
+
+  /* POSTINCREMENT (signature distinguishes the iterators), 
+                    call:  it++, advances one element forward*/
+  ListIterator<T> operator++(int) {
+    if(nullptr == node) {
+      throw "Iterator does not point to valid node";
+    }
+
+    //TODO: Implement Postincrement-Operation for Iterator
+    //      (Aufgabe 3.11 - Teil 4)
+
+  }
+
+
+  /* ... */
+  bool operator==(ListIterator<T> const& x) const {
+    //TODO: Implement Equality-Operation for Iterator
+    //      (Aufgabe 3.11 - Teil 5)
+    // Iterators should be the same if they refer to the same node
+    return false;
+  } // call it: == it
+
+  /* ... */
+  bool operator!=(ListIterator<T> const& x) const {
+    //TODO: Implement Inequality-Operation for Iterator  
+    //      (Aufgabe 3.11 - Teil 6)
+    // Reuse operator==
+    return false;
+  } // call it: != it
+
+  /* Advances Iterator */
+  ListIterator<T> next() const {
+    if (nullptr != node) {
+      return ListIterator{node->next};
+    } else {
+      return ListIterator{nullptr};
+    }
+  }
+
+
+  ListNode <T>* node = nullptr;
+};
+
+
+
+template <typename T>
+class List {
+  public:
+
+    //friend declarations for testing the members   
+    template <typename TEST_TYPE>
+    friend size_t get_size(List<TEST_TYPE> const& list_to_test);
+    template <typename TEST_TYPE>
+    friend ListNode<TEST_TYPE>* get_first_pointer(List<TEST_TYPE> const& list_to_test);
+    template <typename TEST_TYPE>
+    friend ListNode<TEST_TYPE>* get_last_pointer(List<TEST_TYPE> const& list_to_test);
+
+    using value_type      = T;
+    using pointer         = T*;
+    using const_pointer   = T const*;
+    using reference       = T&;
+    using const_reference = T const&;
+    using iterator        = ListIterator<T>;
+
+    // not fully implemented yet
+    // TODO: do not forget about the initialiser list! (Aufgabe 3.2)
+    /* ... */
+    List() {}
+
+    // test and implement:
+    //TODO: Copy-Konstruktor using Deep-Copy semantics (Aufgabe 3.5)
+
+
+
+    //TODO: Initializer-List Konstruktor (3.14 - Teil 1)
+    /* ... */
+    // test and implement:
+    List(std::initializer_list<T> ini_list) {
+      //not implemented yet
+    }
+
+    // test and implement:
+    //TODO: Copy-Konstruktor using Deep-Copy semantics (Aufgabe 3.5)
+
+    /* ... */
+    // test and implement:
+    //TODO: (unifying) Assignment operator (Aufgabe 3.6)
+
+    /* ... */
+    // test and implement:
+
+    bool operator==(List const& rhs)
+    {
+      //TODO: operator== (Aufgabe 3.8)
+    }
+
+    bool operator!=(List const& rhs)
+    {
+      //TODO: operator!= (Aufgabe 3.8)
+      // make use of operator==
+    }
+
+    /* ... */
+    ~List() {
+      //TODO: Implement via clear-Method (Aufgabe 3.4)
+    } //can not really be tested
+
+    /* ... */
+    ListIterator<T> begin() {
+      //TODO: begin-Method returning an Iterator to the 
+      //      first element in the List (Aufgabe 3.10)
+      return {};
+    }
+
+    /* ... */
+    ListIterator<T> end() {
+      //TODO: end-Method returning an Iterator to element after (!) 
+      //      the last element in the List (Aufgabe 3.10)
+      return {};
+    }
+
+    /* ... */ 
+    // test and implement:
+    //TODO: clear()-Method (Aufgabe 3.4)
+
+
+    /* ... */
+    //TODO: member function insert (Aufgabe 3.12)
+
+    /* ... */
+    //TODO: member function insert (Aufgabe 3.13)
+
+    /* ... */
+
+    //TODO: member function reverse (Aufgabe 3.7 - Teil 1)
+
+
+    /* ... */
+    void push_front(T const& element) {
+      // TODO: push_front-method (Aufgabe 3.3)
+    }
+
+    /* ... */
+    void push_back(T const& element) {
+      // TODO: push_back-method (Aufgabe 3.3)
+    }
+
+    /* ... */
+    void pop_front() {
+      if(empty()) {
+        throw "List is empty";
+      }
+
+      // TODO: remainder of pop_front-method (Aufgabe 3.3)
+    }
+
+    /* ... */
+    void pop_back() {
+      if(empty()) {
+        throw "List is empty";
+      }
+
+      // TODO: remainder of pop_back-method (Aufgabe 3.3)
+    }
+
+    /* ... */
+    T& front() {
+      if(empty()) {
+        throw "List is empty";
+      }
+
+      // TODO: remainder of front-method (Aufgabe 3.3)
+    }
+
+    /* ... */
+    T& back() {
+      if(empty()) {
+        throw "List is empty";
+      }
+
+      // TODO: remainder of back-method (Aufgabe 3.3)
+    }
+
+    /* ... */
+    bool empty() const {
+
+      // TODO: empty-method (Aufgabe 3.2)
+      return false;
+    };
+
+
+    /* ... */
+    std::size_t size() const{
+      // TODO: size-method (Aufgabe 3.2)      
+      return 27;
+  };
+
+
+  // list members
+  private: 
+    std::size_t size_;
+    ListNode<T>* first_;
+    ListNode<T>* last_;
+};
+
+/* ... */
+//TODO: Freie Funktion reverse 
+//(Aufgabe 3.7 - Teil 2, benutzt Member-Funktion reverse)
+
+/* ... */
+//TODO: Freie Funktion operator+ (3.14 - Teil 2)
+
+#endif // # define BUW_LIST_HPP
---
+++ source/tests.cpp
@@ -0,0 +1,53 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+
+#include "list.hpp"
+
+
+// Hilfsfunktionen, um indirekt auf die Member der Klasse List zuzugreifen (nur für die Tests!)
+template <typename T>
+size_t get_size(List<T> const& list_to_test) {return list_to_test.size_;}
+
+template <typename T>
+ListNode<T>* get_first_pointer(List<T> const& list_to_test) {return list_to_test.first_;}
+
+template <typename T>
+ListNode<T>* get_last_pointer(List<T> const& list_to_test) {return list_to_test.last_;}
+
+
+//test case definitions for default constructor of list as well as empty and size for empty list
+#include "sub_tests/default_constructed_list.test"
+
+//test case definitions for push methods of list
+#include "sub_tests/push_front.test"
+#include "sub_tests/push_back.test"
+
+//test case definitions for pop methods of list
+#include "sub_tests/pop_front.test"
+#include "sub_tests/pop_back.test"
+
+//test case definitions for 
+
+//test cases for element access of list
+#include "sub_tests/front.test"
+#include "sub_tests/back.test"
+
+//test cases for retrieving iterators
+#include "sub_tests/begin.test"
+#include "sub_tests/end.test"
+
+//iterator tests
+#include "sub_tests/iterators/operator_star.test"
+#include "sub_tests/iterators/operator_arrow.test"
+#include "sub_tests/iterators/operator_equals.test"
+#include "sub_tests/iterators/operator_does_not_equal.test"
+#include "sub_tests/iterators/operator_iterate_forward.test"
+
+int main(int argc, char *argv[])
+{
+  return Catch::Session().run(argc, argv);
+}
+
+
+
+
