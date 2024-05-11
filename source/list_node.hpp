a74d13d0ebcc79aab7d41fe380909a267712a1c4
Nyran <adrian.kreskowski@uni-weimar.de>
2024-05-11 10:09:22 +0200

    comment list node struct

--- source/list_node.hpp
+++ source/list_node.hpp
@@ -1,6 +1,9 @@
 #ifndef BUW_LIST_NODE_HPP
 #define BUW_LIST_NODE_HPP
 
+// complete list node struct, similar to the one presented in lecture "Verwaltung dynamischer Ressourcen (V 6),
+// just as template struct
+
 template <typename T>
 struct ListNode {
 	T         value = T{};
